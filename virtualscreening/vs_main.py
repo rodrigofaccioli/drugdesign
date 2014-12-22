7e94b0b1b2ebbeebec984fe373bae4ff8dc44ce9
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-12-22 11:43:27 -0200

    Fixed to run N receptores

--- virtualscreening/vs_main.py
+++ virtualscreening/vs_main.py
@@ -17,12 +17,12 @@ def main():
 	all_receptor = vina.get_files_pdbqt(config.get('DEFAULT', 'pdbqt_receptor_path'))
 	for receptor in all_receptor:
 		print receptor
-	vina.run_docking(config.get('VINA', 'vina_program'), 
-		config.get('VINA', 'config_file'), 
-		receptor, 
-		config.get('DEFAULT', 'pdbqt_ligand_path'),
-		config.get('DEFAULT', 'path_save_structure'),
-		config.get('DEFAULT', 'path_save_log')
-		) 
+		vina.run_docking(config.get('VINA', 'vina_program'), 
+			config.get('VINA', 'config_file'), 
+			receptor, 
+			config.get('DEFAULT', 'pdbqt_ligand_path'),
+			config.get('DEFAULT', 'path_save_structure'),
+			config.get('DEFAULT', 'path_save_log')
+			) 
 
 main()
