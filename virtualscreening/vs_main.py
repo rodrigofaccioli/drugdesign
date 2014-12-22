90c7d582a9b4ce85340da036cc5da6108b5de5ce
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2014-12-22 19:33:23 -0200

    Added enviroment preparation

--- virtualscreening/vs_main.py
+++ virtualscreening/vs_main.py
@@ -8,13 +8,25 @@
 
 import vina
 import ConfigParser as configparser
+import vs_preparation as prep
 from analysis import call_analysis as ana
 
 def main():
 	config = configparser.ConfigParser()
 	config.read('config.ini')
 
+	#Preparing the virtual screening enviroment
+	prep.preparing_vs(config.get('DEFAULT', 'path_save_structure'),
+		config.get('DEFAULT', 'path_save_log'),
+		config.get('DEFAULT', 'path_analysis'),
+		config.get('DEFAULT', 'pdbqt_ligand_path'),
+		config.get('DEFAULT', 'pdbqt_receptor_path')
+		)
+
+	#Obtain all receptors to perform the virtual screening
 	all_receptor = vina.get_files_pdbqt(config.get('DEFAULT', 'pdbqt_receptor_path'))
+
+	#Performing the the virtual screening for each receptor
 	for receptor in all_receptor:
 		print receptor
 		vina.run_docking(config.get('VINA', 'vina_program'), 
