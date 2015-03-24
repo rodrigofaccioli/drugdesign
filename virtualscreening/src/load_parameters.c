f74fe27adcab4720ebfb4cc3b439e27797227b96
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2015-03-24 15:53:13 -0300

    Added remove_character_enter function and new fields

--- virtualscreening/src/load_parameters.c
+++ virtualscreening/src/load_parameters.c
@@ -23,6 +23,14 @@ static void set_parameters(input_parameters_t *param, const char *key,
 		strcpy(param->path_receptors, value);
 	}else if (strncmp(key,"Path_compounds",14) == 0){
 		strcpy(param->path_compounds, value);
+	}else if (strncmp(key,"Path_out",8) == 0){
+		strcpy(param->path_out, value);
+	}else if (strncmp(key,"Path_log",8) == 0){
+		strcpy(param->path_log, value);
+	}else if (strncmp(key,"Config_file",11) == 0){
+		strcpy(param->config_vina, value);
+	}else if (strncmp(key,"Vina_program",12) == 0){
+		strcpy(param->vina_program, value);
 	}else{
 		fatal_error("Parameter not found\n");
 	}
@@ -45,7 +53,8 @@ void load_parameters_from_file(input_parameters_t *in_param,
 	while (fgets(line, MAX_LINE_FILE, conf) != NULL){		
 		ch = strtok(line, "=");
 		while (ch != NULL) {
-			ltrim(ch);
+			trim(ch);
+			remove_character_enter(ch);
   			if (key_r == 0){
   				strcpy(key, ch);
   				key_r = 1;  				
