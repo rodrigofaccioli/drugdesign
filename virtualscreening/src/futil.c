168c18bf51e549d2951bab479fac72506654c155
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2015-04-05 14:56:15 -0300

    Added functions for manupilating of directory and files

--- virtualscreening/src/futil.c
+++ virtualscreening/src/futil.c
@@ -2,11 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <dirent.h>
 
 #include "messages.h"
 #include "defines.h"
 #include "futil.h"
 #include "string_owner.h"
+
+owner_file_t *allocate_owner_file(const int *size){
+    int i;
+    owner_file_t * aux = NULL;
+    aux = (owner_file_t*)malloc(sizeof(owner_file_t)* *size);
+    for (i=0; i < *size; i++){
+        aux[i].file_name = (char*)malloc(sizeof(char)*MAX_FILE_NAME);
+    }
+    return aux;
+}
+
+void desallocate_owner_file(owner_file_t *aux, const int *size){
+    int i;
+    for (i=0; i < *size; i++){
+        free(aux[i].file_name);
+    }
+    free(aux);
+}
+
+void show_owner_file(const owner_file_t *aux, const int *size){
+    int i;
+    for (i=0; i < *size; i++){
+        printf("%s\n", aux[i].file_name);        
+    }    
+}
+
 
 char * path_join_file(const char *path, const char *f){
 	char *path_file;
@@ -99,4 +126,64 @@ char *get_last_line(const char *filepath){
         --i;
     ++i;
     return strdup(&buff[i]);
+}
+
+const char *get_filename_ext(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return "";
+    return dot + 1;
+}
+
+/* How Many files are at directory by extension
+*/
+int how_many_files_directory_by_extension(const char *path, const char *ext){   
+    const char *ext_aux = NULL; 
+    struct dirent *ent = NULL;
+    int r = 0;
+
+    DIR *dir = opendir(path);
+    while (ent = readdir(dir)) {
+        ext_aux = get_filename_ext(ent->d_name);
+        if (strcmp (ext_aux, ext) == 0){
+            r++;
+        }   
+    }
+    closedir(dir);
+    return r;
+}
+
+void insert_files_directory_by_extension(owner_file_t *file_names, const char *path, const char *ext){
+    const char *ext_aux = NULL;
+    int index = -1;
+
+    DIR *dir = opendir(path);
+    struct dirent *ent;
+    while (ent = readdir(dir)) {
+        ext_aux = get_filename_ext(ent->d_name);
+        if (strcmp (ext_aux, ext) == 0){
+            index = index +1;
+            strcpy(file_names[index].file_name, ent->d_name);
+        }   
+    }
+    closedir(dir);
+}
+
+//Set file name without extension
+void set_base_file_name(char *filename_no_ext, const char *filename_source, const char *ext){
+    char aux_ext[MAX_SUFIX];
+    char *aux=NULL;
+    char *ch=NULL;
+    aux = (char*)malloc(sizeof(char)*MAX_FILE_NAME);
+    strcpy(aux_ext, ".");
+    strcat(aux_ext, ext);
+    strcpy(aux, filename_source);
+    ch=strrchr(aux,'.');
+    substring(filename_no_ext, filename_source, 0, strlen(filename_source)-strlen(ch));
+    free(aux);
+}
+
+//Add extension in file name
+void add_ext_file_name(char *filename, const char *ext){
+    strcat(filename, ".");
+    strcat(filename, ext);
 }
