5eea9d9ff8388bd8ac063ce3e098b817ff7f405c
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2015-04-05 14:57:12 -0300

    Added prepare ligand sequencial

--- virtualscreening/include/vina.h
+++ virtualscreening/include/vina.h
@@ -7,5 +7,6 @@
 void initialize_vina_execution();
 void finish_vina_execution();
 void call_vina(const input_parameters_t *param, const docking_t *docking);
+void call_prepare_ligand(const input_parameters_t *param, const char *fmol2, const char *fpdbqt);
 
 #endif
--- virtualscreening/src/vina.c
+++ virtualscreening/src/vina.c
@@ -22,6 +22,13 @@ static char *file_log_name = NULL;
 static char *file_receptor_name = NULL;
 static char *file_ligand_name = NULL;
 static char *file_out_name = NULL;
+static char *opt_l = NULL;
+static char *opt_v = NULL;
+static char *opt_u = NULL;
+static char *opt_a = NULL;
+static char *opt_o = NULL;
+static char *opt_nphs_lps = NULL;
+static char *opt_hydrogens = NULL;
 
 static inline int run_vina_program(const char *file, char *const argv[]){
   int out, err; /* file descriptors for stdout and stderr */
@@ -62,12 +69,28 @@ void initialize_vina_execution(){
   file_ligand_name = (char*)malloc(sizeof(char)*MAX_FILE_NAME);
   file_out_name = (char*)malloc(sizeof(char)*MAX_FILE_NAME);
 
+  opt_l = (char*)malloc(sizeof(char)*3);
+  opt_v = (char*)malloc(sizeof(char)*3);
+  opt_u = (char*)malloc(sizeof(char)*3);
+  opt_a = (char*)malloc(sizeof(char)*3);
+  opt_o = (char*)malloc(sizeof(char)*3);
+  opt_nphs_lps = (char*)malloc(sizeof(char)*10);
+  opt_hydrogens = (char*)malloc(sizeof(char)*11);
+
+
   //setting option values
   strcpy(opt_config, "--config");
   strcpy(opt_receptor, "--receptor");
   strcpy(opt_ligand, "--ligand");
   strcpy(opt_out, "--out");
   strcpy(opt_log, "--log");
+  strcpy(opt_l, "-l");
+  strcpy(opt_v, "-v");
+  strcpy(opt_u, "-U");
+  strcpy(opt_a, "-A");
+  strcpy(opt_o, "-o");
+  strcpy(opt_nphs_lps, "nphs_lps");
+  strcpy(opt_hydrogens, "hydrogens");
 }
 
 void finish_vina_execution(){
@@ -87,6 +110,14 @@ void finish_vina_execution(){
   free(file_receptor_name);
   free(file_ligand_name);
   free(file_out_name);
+  free(opt_l);
+  free(opt_v);
+  free(opt_u);
+  free(opt_a);
+  free(opt_o);
+  free(opt_nphs_lps);
+  free(opt_hydrogens);
+
 }
 
 /* Setting log file name
@@ -166,3 +197,44 @@ void call_vina(const input_parameters_t *param, const docking_t *docking){
 
 }
 
+
+
+void call_prepare_ligand(const input_parameters_t *param, const char *fmol2, const char *fpdbqt){
+    char *ligand_args[12];
+    char *script_ligand4=NULL;
+    char *mol2_aux = NULL;
+    char *pdbqt_aux = NULL;
+
+    script_ligand4 = (char*)malloc(sizeof(char)*MAX_PATH_FILE_NAME);
+    mol2_aux = (char*)malloc(sizeof(char)*MAX_PATH_FILE_NAME);
+    pdbqt_aux = (char*)malloc(sizeof(char)*MAX_PATH_FILE_NAME);
+
+    strcpy(program, param->pythonsh);
+    ligand_args[0] = program;
+    //script ligand4
+    strcpy(script_ligand4, param->script_ligand4);
+    ligand_args[1] = script_ligand4;
+    ligand_args[2] = opt_l;
+    //mol2 file
+    strcpy(mol2_aux, fmol2);
+    ligand_args[3] = mol2_aux;
+    ligand_args[4] = opt_v;
+    ligand_args[5] = opt_o;
+    //pdbqt file
+    strcpy(pdbqt_aux, fpdbqt);
+    ligand_args[6] = pdbqt_aux;
+    ligand_args[7] = opt_u;
+    ligand_args[8] = opt_nphs_lps;
+    ligand_args[9] = opt_a;
+    ligand_args[10] = opt_hydrogens;
+
+    ligand_args[11] = NULL;
+
+    if (!run_vina_program(program, ligand_args) ){
+      fatal_error("Failed to prepare ligand \n");     
+    }
+
+    free(mol2_aux);
+    free(pdbqt_aux);
+    free(script_ligand4);
+}
