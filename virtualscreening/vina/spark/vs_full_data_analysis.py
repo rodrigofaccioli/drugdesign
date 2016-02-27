758546c6380a77dfa69a3790739cec9629f64cba
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2016-02-27 15:57:33 -0300

    Added Hydrogen Bind at full data file

--- virtualscreening/vina/spark/vs_full_data_analysis.py
+++ virtualscreening/vina/spark/vs_full_data_analysis.py
@@ -5,13 +5,19 @@ import os
 from vina_utils import get_file_name_sorted_energy
 from datetime import datetime
 
+def format_Hydrogen_bind(num_hydrogen_bind):
+	if num_hydrogen_bind == None:
+		return "0.0000"
+	else:
+		return str("{:.4f}".format(num_hydrogen_bind) )
+
 def save_vs_full_data(path_analysis, list_full_data, full_data_file_name):	
 	path_file_vs_full_data = os.path.join(path_analysis, full_data_file_name)
 	f_vs_full_data = open(path_file_vs_full_data,"w")
-	header = ";receptor\tligand\t\tmode\ttorsion\tatom\theavyA\tafinity\tefficiency\tb_lig_rec_perc\tb_lig_lig_perc\n"
+	header = ";receptor\tligand\t\tmode\ttorsion\tatom\theavyA\tafinity\tefficiency\tb_lig_rec_perc\tb_lig_lig_perc\tnum_Hydrogen_Bind\n"
 	f_vs_full_data.write(header)	
 	for full_data in list_full_data:
-		line = str(full_data[0])+"\t"+str(full_data[1])+"\t"+str(full_data[2])+"\t"+str(full_data[3])+"\t"+str(full_data[4])+"\t"+str(full_data[5])+"\t"+str(full_data[6])+"\t"+str("{:.4f}".format(full_data[7]))+"\t"+str("{:.4f}".format(full_data[8]))+"\t"+str("{:.4f}".format(full_data[9]))+"\n"
+		line = str(full_data[0])+"\t"+str(full_data[1])+"\t"+str(full_data[2])+"\t"+str(full_data[3])+"\t"+str(full_data[4])+"\t"+str(full_data[5])+"\t"+str(full_data[6])+"\t"+str("{:.4f}".format(full_data[7]))+"\t"+str("{:.4f}".format(full_data[8]))+"\t"+str("{:.4f}".format(full_data[9]))+"\t"+ format_Hydrogen_bind(full_data[10]) +"\n"
 		f_vs_full_data.write(line)
 	f_vs_full_data.close()
 
@@ -102,12 +108,25 @@ def main():
 	ligand_efficiency_table.registerTempTable("ligandEfficiency")
 #**************** Finish	
 
+#**************** Loading Hydrogen Bind 
+	hydrogen_bind_num_pose_file_name = os.path.join(path_analysis,"hbonds_number_pose_4.0")
+	hydrogen_bind_num_pose_file = sc.textFile(hydrogen_bind_num_pose_file_name)
+
+	#Spliting file by \t
+	rdd_hydrogen_bind_split = hydrogen_bind_num_pose_file.map(lambda line: line.split("\t"))
+	rdd_hydrogen_bind = rdd_hydrogen_bind_split.map(lambda p: Row(receptor=str(p[0]),ligand=str(p[1]), mode=int(p[2]), numHydroBind=float(p[3]) ) )
+	#Creating buried Dataframe
+	hydrogen_bind_table = sqlCtx.createDataFrame(rdd_hydrogen_bind)	
+	hydrogen_bind_table.registerTempTable("hydrogenbind")
+#**************** Finish	
+
 	#Creating SQL command
 	sql = ""
 	sql = "SELECT database.ligand, database.torsion, database.atom, database.heavyAtom"
 	sql +=" ,vina.receptor, vina.mode, vina.energy"
 	sql +=" ,buriedArea.sasa_lig_min, buriedArea.sasa_lig_pose, buriedArea.sasa_lig_complex, buriedArea.buried_lig_rec_perc, buriedArea.buried_lig_lig_perc"
 	sql +=" ,ligandEfficiency.lig_eff "
+	sql +=" ,hydrogenbind.numHydroBind "
 	sql +=" FROM database"
 	sql +=" JOIN vina ON vina.ligand = database.ligand"
 	sql +=" JOIN buriedArea ON buriedArea.ligand = database.ligand AND "
@@ -115,12 +134,16 @@ def main():
 	sql +="                    buriedArea.mode = vina.mode"
 	sql +=" JOIN ligandEfficiency ON ligandEfficiency.ligand = database.ligand AND "
 	sql +="                    ligandEfficiency.receptor = vina.receptor AND"
-	sql +="                    ligandEfficiency.mode = vina.mode"	
+	sql +="                    ligandEfficiency.mode = vina.mode"
+	sql +=" LEFT OUTER	"
+	sql +=" JOIN hydrogenbind ON hydrogenbind.ligand = database.ligand AND "
+	sql +="                    hydrogenbind.receptor = vina.receptor AND"
+	sql +="                    hydrogenbind.mode = vina.mode"
 	sql +=" ORDER BY vina.receptor, vina.ligand, vina.mode"
 
 	#Getting all data
 	full_dataRDD = sqlCtx.sql(sql) 
-	full_dataRDD = full_dataRDD.map(lambda p: (p.receptor, p.ligand, p.mode, p.torsion, p.atom, p.heavyAtom, p.energy, p.lig_eff, p.buried_lig_rec_perc, p.buried_lig_lig_perc) ).collect()
+	full_dataRDD = full_dataRDD.map(lambda p: (p.receptor, p.ligand, p.mode, p.torsion, p.atom, p.heavyAtom, p.energy, p.lig_eff, p.buried_lig_rec_perc, p.buried_lig_lig_perc, p.numHydroBind) ).collect()
 
 	#Saving file
 	save_vs_full_data(path_analysis, full_dataRDD, full_data_file_name)	
