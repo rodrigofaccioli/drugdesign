88320fec78dc4247753513bdf171ceab23618209
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2016-06-16 11:34:20 -0300

    Updated the calculation of area receptor

--- virtualscreening/vina/spark/buried_area_receptor.py
+++ virtualscreening/vina/spark/buried_area_receptor.py
@@ -10,7 +10,29 @@ from gromacs_utils import get_value_from_xvg_sasa
 from pdb_io import replace_chain_atom_line
 import shutil
 
-def sorting_buried_area_recep(sc, buried_areaRDD):
+def sorting_buried_area_receptor(sc, buried_areaRDD):
+	sqlCtx = SQLContext(sc)
+	buried_areaRDD = sc.parallelize(buried_areaRDD)
+	buried_areaRDD = buried_areaRDD.map(lambda p: Row(pose=str(p[0]), buried_area_rec=float(p[1]) ) )
+	buried_area_table = sqlCtx.createDataFrame(buried_areaRDD)	
+	buried_area_table.registerTempTable("buried_area_receptor")
+
+	buried_area_sorted_by_recep = sqlCtx.sql("SELECT * FROM buried_area_receptor ORDER BY buried_area_rec ") 
+	return buried_area_sorted_by_recep
+
+def save_buried_area_receptor_sort(path_file_buried_area, buried_area):	
+	f_buried_area = open(path_file_buried_area,"w")
+	line = "# buried_area_receptor[nm2]\tpose"+"\n"
+	f_buried_area.write(line)	
+	for area in buried_area:
+		pose = str(str(area[0]).replace("compl_", " ")).strip()
+		buried_lig_rec = "{:.4f}".format(area[1])
+		line = str(buried_lig_rec)+"\t"+str(pose)+"\n"
+		f_buried_area.write(line)			
+	f_buried_area.close()
+
+
+def sorting_buried_area_all_residues(sc, buried_areaRDD):
 	sqlCtx = SQLContext(sc)
 	buried_areaRDD = sc.parallelize(buried_areaRDD)
 	#buried_areaRDD = buried_areaRDD.map(lambda p: Row(receptor=str(p[0]), ligand=str(p[1]), model=int(p[2]), res=str(p[3]), res_buried_area=float(p[4]), res_buried_area_perc=float(p[5]) ) )
@@ -67,11 +89,26 @@ def loading_lines_from_recepArea_files(line):
 	line_ret = ( str(line_splited[0]), float(line_splited[1]), float(line_splited[2]), str(line_splited[3]) )	
 	return line_ret
 
+def loading_lines_from_outAreaRecep_files(line):
+	line_splited = str(line).split()
+	line_ret = ( str(line_splited[0]), float(line_splited[1]) )	
+	return line_ret
+
 def get_files_recepArea(mypath):
 	only_mol2_file = []
 	for root, dirs, files in os.walk(mypath):
 		for file in files:
 			if file.endswith(".recepArea"):
+				f_path = os.path.join(root,file)
+				only_mol2_file.append(f_path)			
+	return only_mol2_file
+
+
+def get_files_outAreaRecep(mypath):
+	only_mol2_file = []
+	for root, dirs, files in os.walk(mypath):
+		for file in files:
+			if file.endswith(".outAreaRecep"):
 				f_path = os.path.join(root,file)
 				only_mol2_file.append(f_path)			
 	return only_mol2_file
@@ -136,8 +173,7 @@ def main():
 	sc.addPyFile(os.path.join(path_spark_drugdesign,"gromacs_utils.py"))
 	sc.addPyFile(os.path.join(path_spark_drugdesign,"pdb_io.py"))
 
-	#Adding bash scripts
-	sc.addFile(os.path.join(path_spark_drugdesign,"make_ndx.sh"))
+	#Adding bash scripts	
 	sc.addFile(os.path.join(path_spark_drugdesign,"make_ndx_buried_area_receptor.sh"))
 	sc.addFile(os.path.join(path_spark_drugdesign,"make_ndx_buried_area_receptor_res.sh"))	
 
@@ -194,7 +230,7 @@ def main():
 # ********** Finish function **********************************************************					
 
 # ********** Starting function **********************************************************		
-		def compute_buried_area_receptor(pdb_complex):
+		def compute_buried_area_all_residues_and_receptor_area(pdb_complex):
 			chZ = "chZ"
 			res_buried_area_perc = -1
 			res_buried_area = -1
@@ -204,14 +240,20 @@ def main():
 			ligand_name = get_ligand_from_receptor_ligand_model(base_name)
 			receptor_name = get_receptor_from_receptor_ligand_model(base_name)
 			pose = get_model_from_receptor_ligand_model(base_name)			
+			
+			#output area receptor file
+			f_output_receptor_buried_area = os.path.join(path_analysis_pdb_complex_b.value,base_name+".outAreaRecep")
 			#ndx files					
-			f_ndx = os.path.join(path_analysis_pdb_complex_b.value,base_name+".ndx")			
+			#f_ndx = os.path.join(path_analysis_pdb_complex_b.value,base_name+".ndx")			
 			f_ndx_temporary_index_z = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary_index_z"+".ndx")
 			f_ndx_temporary = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary"+".ndx")			
 			f_ndx_temporary_sasa = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary_sasa"+".ndx")			
+
 			#xvg files
 			f_xvg_temporary_sasa_res_lig = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary_sasa_res-lig"+".xvg")
 			f_xvg_temporary_sasa_res  = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary_sasa_res"+".xvg")
+			f_xvg_temporary_sasa_rec_lig  = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary_sasa_rec_lig"+".xvg")
+			f_xvg_temporary_sasa_rec  = os.path.join(path_analysis_pdb_complex_b.value,base_name+"_temporary_sasa_rec"+".xvg")
 
 			# Creates a selection with the residues that are closer than 6A to the ligand
 			script_make_ndx_buried_area_receptor = SparkFiles.get("make_ndx_buried_area_receptor.sh") #Getting bash script that was copied by addFile command
@@ -246,13 +288,33 @@ def main():
 					result = (base_name, res, res_buried_area,  res_buried_area_perc)				
 					returned_list.append(result)
 				#Deleting files
-				#os.remove(f_xvg_temporary_sasa_res_lig)
-				#os.remove(f_xvg_temporary_sasa_res)				
+				os.remove(f_xvg_temporary_sasa_res_lig)
+				os.remove(f_xvg_temporary_sasa_res)
 
-			#Deleting file
-			#os.remove(f_ndx_temporary)
-			#os.remove(f_ndx_temporary_sasa)
-			#os.remove(f_ndx_temporary_index_z)
+			#Computing Receptor Area
+			command = gromacs_path.value +"gmx sasa -surface complex -output rec"+ " -o "+ f_xvg_temporary_sasa_rec_lig + " -xvg none -f " + pdb_complex +" -s " + pdb_complex + " -n "+ f_ndx_temporary + " -nopbc "			
+			process = Popen(command,shell=True, stdout=PIPE, stderr=PIPE)
+			stdout, stderr = process.communicate()			
+
+			command = gromacs_path.value +"gmx sasa -surface rec -output rec"+ " -o "+ f_xvg_temporary_sasa_rec + " -xvg none -f " + pdb_complex +" -s " + pdb_complex + " -n "+ f_ndx_temporary + " -nopbc "			
+			process = Popen(command,shell=True, stdout=PIPE, stderr=PIPE)
+			stdout, stderr = process.communicate()			
+
+			sasa_rec_lig = get_value_from_xvg_sasa(f_xvg_temporary_sasa_rec_lig)
+			sasa_rec = get_value_from_xvg_sasa(f_xvg_temporary_sasa_rec)
+			receptor_area = sasa_rec - sasa_rec_lig
+			#Saving result file
+			output_receptor_buried_area = open(f_output_receptor_buried_area, "w")
+			output_receptor_buried_area.write(str(base_name)+" "+str(receptor_area) +"\n")
+			output_receptor_buried_area.close()
+
+			#Deleting all files
+			os.remove(f_xvg_temporary_sasa_rec_lig)
+			os.remove(f_xvg_temporary_sasa_rec)
+			os.remove(f_ndx_temporary)
+			os.remove(f_ndx_temporary_sasa)
+			os.remove(f_ndx_temporary_index_z)
+
 			return returned_list				
 			
 # ********** Finish function **********************************************************					
@@ -268,23 +330,23 @@ def main():
 			full_path_for_save_complex = os.path.join(path_pdb_complex,complex_name)
 			list_receptor_model_file = (model_file, full_path_for_save_complex)						
 			save_model_receptor(list_receptor_model_file)			
-			list_ret = compute_buried_area_receptor(full_path_for_save_complex)			
+			list_ret = compute_buried_area_all_residues_and_receptor_area(full_path_for_save_complex)			
 			os.remove(full_path_for_save_complex)
 			return list_ret
 # ********** Finish function **********************************************************	
-
+		
+		#Computing buried area of All-residues and receptor
 		all_model_filesRDD = sc.parallelize(all_model_filesRDD)
-		all_model_filesRDD = all_model_filesRDD.map(build_list_model_for_complex).collect()	
-		#Saving buried area of residue receptor
+		all_model_filesRDD = all_model_filesRDD.map(build_list_model_for_complex).collect()			
 		full_area_file  = os.path.join(path_analysis,base_file_name_receptor+".recepArea")
 		save_receptor_buried_area_receptor(full_area_file, all_model_filesRDD)
 
-	#Loading all area file 
+# ***************** Starting ******************************************/
+	#Loading All-residues files 
 	all_area_file = os.path.join(path_analysis,"*.recepArea")		
 	buried_areaRDD = sc.textFile(all_area_file).map(loading_lines_from_recepArea_files).collect()
-
 	#Sorting by res_buried_area_perc column
-	buried_area_sorted_by_res_buried_area_perc = sorting_buried_area_recep(sc, buried_areaRDD)
+	buried_area_sorted_by_res_buried_area_perc = sorting_buried_area_all_residues(sc, buried_areaRDD)
 	buried_area_sorted_by_res_buried_area_perc = buried_area_sorted_by_res_buried_area_perc.map(lambda p: (p.res, p.res_buried_area, p.res_buried_area_perc, p.pose) ).collect() #p.receptor, p.ligand, p.model,
 
 	#Saving buried area file
@@ -295,6 +357,24 @@ def main():
 	all_area_files = get_files_recepArea(path_analysis)
 	for area_file in all_area_files:
 		os.remove(area_file)
+# ***************** Finish ******************************************/
+
+# ***************** Starting ******************************************/
+
+	#Loading outAreaRecep files
+	all_outAreaRecep_file = os.path.join(path_analysis_pdb,"*.outAreaRecep")		
+	buried_outAreaRecepRDD = sc.textFile(all_outAreaRecep_file).map(loading_lines_from_outAreaRecep_files).collect()
+
+	#Saving buried area receptor file
+	path_file_buried_area_rec = os.path.join(path_analysis, "summary_buried_areas_receptor.dat")
+	save_buried_area_receptor_sort(path_file_buried_area_rec, buried_outAreaRecepRDD)	
+
+	#Removing all outAreaRecep files
+	all_outAreaRecep = get_files_outAreaRecep(path_analysis)
+	for outAreaRecep in all_outAreaRecep:
+		os.remove(outAreaRecep)
+# ***************** Finish ******************************************/
+
 
 	finish_time = datetime.now()
 
