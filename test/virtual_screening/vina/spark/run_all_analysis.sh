06009b92b29bcb7e57e1988502a290da7f5dc4eb
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2016-06-17 00:13:46 -0300

    Added driver memory

--- test/virtual_screening/vina/spark/run_all_analysis.sh
+++ test/virtual_screening/vina/spark/run_all_analysis.sh
@@ -3,18 +3,18 @@
 spark_paths=$1
 path_files=$2
 
-$spark_paths $path_files"/"create_file_for_analysis.py
+$spark_paths --driver-memory 20g $path_files"/"create_file_for_analysis.py
 
-$spark_paths $path_files"/"prepare_files_for_analysis.py
+$spark_paths --driver-memory 20g $path_files"/"prepare_files_for_analysis.py
 
-$spark_paths $path_files"/"ligand_efficiency.py
+$spark_paths --driver-memory 20g $path_files"/"ligand_efficiency.py
 
-$spark_paths $path_files"/"buried_areas.py 0.14 24
+$spark_paths --driver-memory 20g $path_files"/"buried_areas.py 0.14 24
 
-$spark_paths $path_files"/"buried_area_receptor.py
+$spark_paths --driver-memory 20g $path_files"/"buried_area_receptor.py
 
-$spark_paths $path_files"/"buried_area_ligand.py  0.14 24
+$spark_paths --driver-memory 20g $path_files"/"buried_area_ligand.py  0.14 24
 
-$spark_paths $path_files"/"hydrogen_bind.py 4.0 30.0
+$spark_paths --driver-memory 20g $path_files"/"hydrogen_bind.py 4.0 30.0
 
-$spark_paths $path_files"/"vs_full_data_analysis.py
+$spark_paths --driver-memory 20g $path_files"/"vs_full_data_analysis.py
