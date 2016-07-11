6e3d1e048dffe07b3c5c99d885b9683ec29db04d
Rodrigo Faccioli <rodrigo.faccioli@gmail.com>
2016-07-11 10:04:11 -0300

    Added running to select by hydrogen bind and buried area

--- test/virtual_screening/vina/spark/run_all_analysis.sh
+++ test/virtual_screening/vina/spark/run_all_analysis.sh
@@ -2,23 +2,24 @@
 
 spark_paths=$1
 path_files=$2
+d_memory=$3"g"
 
-$spark_paths --driver-memory 20g $path_files"/"create_file_for_analysis.py
+$spark_paths --driver-memory $d_memory $path_files"/"create_file_for_analysis.py
 
-$spark_paths --driver-memory 20g $path_files"/"prepare_files_for_analysis.py
+$spark_paths --driver-memory $d_memory $path_files"/"prepare_files_for_analysis.py
 
-$spark_paths --driver-memory 20g $path_files"/"ligand_efficiency.py
+$spark_paths --driver-memory $d_memory $path_files"/"ligand_efficiency.py
 
-$spark_paths --driver-memory 20g $path_files"/"buried_areas.py 0.14 24
+$spark_paths --driver-memory $d_memory $path_files"/"buried_areas.py 0.14 24
 
-$spark_paths --driver-memory 20g $path_files"/"buried_area_receptor.py
+$spark_paths --driver-memory $d_memory $path_files"/"buried_area_receptor.py
 
-$spark_paths --driver-memory 20g $path_files"/"buried_area_ligand.py  0.14 24
+$spark_paths --driver-memory $d_memory $path_files"/"buried_area_ligand.py  0.14 24
 
-$spark_paths --driver-memory 20g $path_files"/"hydrogen_bind.py 4.0 30.0
+$spark_paths --driver-memory $d_memory $path_files"/"hydrogen_bind.py 4.0 30.0
 
-$spark_paths --driver-memory 20g $path_files"/"vs_full_data_analysis.py
+$spark_paths --driver-memory $d_memory $path_files"/"vs_full_data_analysis.py
 
-$spark_paths --driver-memory 20g $path_files"/"buried_area_residue_selection.py
+$spark_paths --driver-memory $d_memory $path_files"/"buried_area_residue_selection.py
 
-$spark_paths --driver-memory 20g $path_files"/"hydrogen_bind_residue_selection.py
+$spark_paths --driver-memory $d_memory $path_files"/"hydrogen_bind_residue_selection.py
